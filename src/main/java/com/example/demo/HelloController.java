359ce6d3d5b1efff0bc5400c68cff747680dcb43
CodenIsCoding <Coden.weber@connect.qut.edu.au>
2025-08-27 22:10:55 +1000

    Student landing page, classes page and profile page

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import javafx.scene.control.Button;
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+
+    @FXML private Button classesButton;
+    @FXML private Button profileButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onClassesButtonClick() throws IOException {
+        Stage stage = (Stage) classesButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("classes-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setScene(scene);
     }
+    @FXML
+    protected void setProfileButtonClick() throws IOException {
+        Stage stage = (Stage) profileButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("profile-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setScene(scene);
+    }
+
+
+
 }
